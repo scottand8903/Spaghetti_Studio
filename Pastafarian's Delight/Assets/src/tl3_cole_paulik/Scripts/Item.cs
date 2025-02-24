4e55ab791bdfd81fab4cd4a59bdcf75b2c085334
Cole Paulik <cole.paulik@gmail.com>
2025-02-23 20:22:10 -0800

    Changes to item

--- Pastafarian's Delight/Assets/src/tl3_cole_paulik/Scripts/Item.cs
+++ Pastafarian's Delight/Assets/src/tl3_cole_paulik/Scripts/Item.cs
@@ -6,19 +6,19 @@ public class ItemUse : MonoBehaviour
     [SerializeField] public int id;
     [SerializeField] public string itemName;
     [SerializeField] public string description;
-    [SerializeField] public bool isAvailable;
+    [SerializeField] public int isAvailable; 
     [SerializeField] public Sprite itemSprite;
-
 
     private void Start()
     {
-        if (isAvailable)
+        Debug.Log("Item " + itemName + " availability: " + isAvailable);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
         {
-            Debug.Log("Item " + itemName + " is available.");
-        }
-        else
-        {
-            Debug.Log("Item " + itemName + " is not available.");
+            Debug.Log("Player collided with " + itemName);
         }
     }
 }
