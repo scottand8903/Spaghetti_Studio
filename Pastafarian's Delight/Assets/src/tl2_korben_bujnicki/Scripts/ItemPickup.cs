ddcd0e9bd7b070c093ba511b6a7de7680507263b
Korben <148403966+korbrokDev@users.noreply.github.com>
2025-02-23 16:12:50 -0800

    Update ItemPickup.cs

--- Pastafarian's Delight/Assets/src/tl2_korben_bujnicki/Scripts/ItemPickup.cs
+++ Pastafarian's Delight/Assets/src/tl2_korben_bujnicki/Scripts/ItemPickup.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public int itemId;
+    public Sprite itemSprite;
+
+    private bool isPlayerNearby = false;
+    private InventorySystem inventorySystem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.P))
+        {
+            TryPickUp();
+        }
     }
+
+    void TryPickUp()
+    {
+        if (inventorySystem != null)
+        {
+            int result = inventorySystem.addItem(itemId, itemSprite);
+            if (result == 1)
+            {
+                Debug.Log("Item picked up and added to inventory!");
+                Destroy(gameObject); // Remove item from the scene
+            }
+            else
+            {
+                Debug.Log("Inventory is full!");
+            }
+        }
+        else
+        {
+            Debug.LogError("InventorySystem not found!");
+        }
+    }
+
+
 }
