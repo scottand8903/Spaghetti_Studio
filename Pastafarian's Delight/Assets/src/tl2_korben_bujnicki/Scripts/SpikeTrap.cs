e28bba2c940965b73eb26e586e5f860f827e9104
Korben <148403966+korbrokDev@users.noreply.github.com>
2025-04-02 21:48:12 -0700

    Spikey trap

---
+++ Pastafarian's Delight/Assets/src/tl2_korben_bujnicki/Scripts/SpikeTrap.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpikyTrap : MonoBehaviour
+{
+    private bool isPlayerTouching = false;
+    private Coroutine damageCoroutine;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (HealthSystem.Instance != null)
+            {
+                HealthSystem.Instance.updateHealth(-1); // Instant damage
+                isPlayerTouching = true;
+                damageCoroutine = StartCoroutine(DealDamageOverTime());
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerTouching = false;
+            if (damageCoroutine != null)
+            {
+                StopCoroutine(damageCoroutine);
+                damageCoroutine = null;
+            }
+        }
+    }
+
+    IEnumerator DealDamageOverTime()
+    {
+        while (isPlayerTouching)
+        {
+            yield return new WaitForSeconds(1f);
+            if (HealthSystem.Instance != null)
+            {
+                HealthSystem.Instance.updateHealth(-1);
+            }
+        }
+    }
+}
+
