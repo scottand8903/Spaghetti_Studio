c59c11aca2058b4e6a7a29ad3b2b097146e103a2
scottand8903 <ande8903@vandals.uidaho.edu>
2025-04-09 20:53:52 -0700

    Updating itemId in Item script

---
+++ Pastafarian's Delight/Assets/src/tl1_scott_anderson/Scripts/PuzzleChecker.cs
@@ -0,0 +1,28 @@
+// using UnityEngine;
+// using System.Collections.Generic;
+
+// public class PuzzleChecker : MonoBehaviour
+// {
+    
+
+//     public void CheckIfCorrect()
+//     {
+//         if(InventorySystem.Instance != null)
+//         {
+//             Debug.Log("InventorySystem instance is not null");
+//             return;
+//         }
+
+//         if(Puzzle.Instance.currentDish == null)
+//         {
+//             Debug.Log("Current dish is null");
+//             return;
+//         }
+
+//         // int[] inventory = InventorySystem.Instance.loadInventory();
+        
+
+//         List<int> correctIngredients = Puzzle.Instance.currentDish.ingredients;
+//     }
+
+// }
