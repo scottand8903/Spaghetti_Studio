6ea99b8f5b1696ca07de32788be9a43f7b844375
scottand8903 <ande8903@vandals.uidaho.edu>
2025-04-09 22:24:02 -0700

    Added Puzzle validator (and stove asset)

--- Pastafarian's Delight/Assets/src/tl1_scott_anderson/Scripts/PuzzleChecker.cs
+++ Pastafarian's Delight/Assets/src/tl1_scott_anderson/Scripts/PuzzleChecker.cs
@@ -1,28 +1,51 @@
-// using UnityEngine;
-// using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
-// public class PuzzleChecker : MonoBehaviour
-// {
-    
+public class PuzzleChecker : MonoBehaviour
+{
 
-//     public void CheckIfCorrect()
-//     {
-//         if(InventorySystem.Instance != null)
-//         {
-//             Debug.Log("InventorySystem instance is not null");
-//             return;
-//         }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckIfCorrect();
+    }
 
-//         if(Puzzle.Instance.currentDish == null)
-//         {
-//             Debug.Log("Current dish is null");
-//             return;
-//         }
+    public void CheckIfCorrect()
+    {
+        if(InventorySystem.Instance == null)
+        {
+            Debug.Log("InventorySystem instance is null");
+            return;
+        }
 
-//         // int[] inventory = InventorySystem.Instance.loadInventory();
-        
+        if(Puzzle.Instance.currentDish == null)
+        {
+            Debug.Log("Current dish is null");
+            return;
+        }
 
-//         List<int> correctIngredients = Puzzle.Instance.currentDish.ingredients;
-//     }
+        int[] inventory = InventorySystem.Instance.GetInventoryIDs();
+        List<int> correctIngredients = Puzzle.Instance.currentDish.ingredients.Select(ing => ing.id).ToList();
 
-// }
+        for(int i = 0; i < 3; i++)
+        {
+            int itemID = inventory[i];
+            if (!correctIngredients.Contains(itemID))
+            {
+                Debug.Log($"Slot {i + 1} contains an incorrect ingredient: {itemID}");
+                Debug.Log("Not all ingredients match.");
+                return;
+            }
+        }
+
+        // If all 3 slots match
+        Debug.Log("All 3 ingredients in the first 3 slots match the current dish!");
+        ConfirmMatch();
+    }
+
+    private void ConfirmMatch()
+    {
+        Debug.Log("Puzzle complete.");
+    }
+
+}
