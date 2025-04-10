1507faab477cac5690aeaefd704d1c90a3845364
Collin Worth <worthcollin@gmail.com>
2025-04-09 21:27:09 -0700

    bug fixes jitter and pickup range

--- Pastafarian's Delight/Assets/src/tl2_collin_worth/Scripts/PlayerController.cs
+++ Pastafarian's Delight/Assets/src/tl2_collin_worth/Scripts/PlayerController.cs
@@ -91,8 +91,9 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
+
         GetPlayerMovement();
         
         bool up = Input.GetKey(KeyCode.W);
