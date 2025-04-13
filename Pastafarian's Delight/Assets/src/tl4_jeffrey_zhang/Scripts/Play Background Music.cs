dec98fb60711792b9d6729ee718232edf49108a8
yayuuuhhh <52010661+yayuuuhhh@users.noreply.github.com>
2025-04-13 01:35:50 -0700

    finished button clicking sound adding
    title

---
+++ Pastafarian's Delight/Assets/src/tl4_jeffrey_zhang/Scripts/Play Background Music.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayBackgroundMusic : MonoBehaviour
+{
+    private static PlayBackgroundMusic instance = null;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
